e7d06f0d482285a19fad7e026230c352cb42ebc5
tabinnn <104389531+tabinnn@users.noreply.github.com>
2022-05-10 00:36:03 +0500

    Add files via upload

---
+++ TestESC.java
@@ -0,0 +1,14 @@
+public class TestESC
+{
+	public static void main(String args[])
+	{
+		System.out.println("Good Morning\t Geeks");
+		System.out.println("Good Morning\bg Geeks");
+		System.out.println("Good Morning\n How are you all?");
+		System.out.println("Good Morning\r How are you all?");
+		System.out.println("Good Morning\f How are you all?");
+		System.out.println("Good Morning\' How are you all?");
+		System.out.println("Good Morning\" How are you all?");
+		System.out.println("Goood Morning\\-this is backslach");
+	}
+}
